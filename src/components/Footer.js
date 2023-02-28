03f18c7b43d9017e491e83a558b974f8ec005d7e
Anjali Ghanshani <anjali.ghanshani@gmail.com>
2023-03-01 00:59:20 +1100

    HTML semantics
    - HTML5 semantic tags make web-pages convey more meaning allowing people, screen reader and serach engine to better understand the site mark-ups

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div>
+                <h3>Doormat Nav</h3>
+                <li>Home</li >
+                <li> About</li >
+                <li> Menu</li >
+                <li> Reservations</li >
+                <li> Order Online</li >
+                <li> Login</li >
+            </div >
+            <div>
+                <h3>Contact</h3>
+                <li>Address</li >
+                <li> phone</li >
+                <li> email</li >
+            </div >
+            <div>
+                <h3>Connect on social media</h3>
+                <li>LinkedIn</li >
+                <li> Instagram</li >
+                <li> Facebook</li >
+                <li> Twitter</li >
+            </div >
+
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <ul>
+                <li><a href="https://google.com" >Home</a></li>
+                <li><a href="https://google.com">About</a></li>
+                <li><a href="https://google.com">Menu</a></li>
+                <li><a href="https://google.com">Reservations</a></li>
+                <li><a href="https://google.com">Order Online</a></li>
+                <li><a href="https://google.com">Login</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav
