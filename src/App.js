13b1396da65baf6825521d662d344fbcc2124c14
Anjali Ghanshani <anjali.ghanshani@gmail.com>
2023-04-03 19:30:20 +1000

    booking form
    - improved booking form layout
    - validation of form inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Header from "./components/Header/Header"
 import Footer from './components/Footer/Footer';
-import CallToAction from './components/CallToAction/CallToAction.js'
-import Specials from "./components/Specials/Specials";
-import CustomersSay from "./components/CustomersSay/CustomersSay";
-import BookingPage from "./components/BookingPage/BookingPage";
-import './App.css'
 
 function App() {
   return (
